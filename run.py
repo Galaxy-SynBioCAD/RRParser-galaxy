5617714b2b3ae370897a03848dee6ae12bc70f2b
joan <joan.herisson@univ-evry.fr>
2020-06-23 17:08:38 +0200

    integrate brs-rr module

--- run.py
+++ run.py
@@ -6,35 +6,24 @@ Created on June 03 2020
 @description: Galaxy script to query RetroRules service
 
 """
-from sys import path as sys_path
-sys_path.insert(0, '/home/src')
-from main import build_parser as tool_buildparser
-from main import entrypoint as tool_entrypoint
-from tarfile import open as tarfile_open
+# from tarfile import open as tarfile_open
 from tempfile import TemporaryDirectory as tempfile_tempdir
-from os import listdir as os_listdir
 from shutil import copy as shutil_copy
-
+from brs_rr import RetroRules, build_parser
 
 if __name__ == "__main__":
 
     # Parse arguments with the tool parser
-    parser = tool_buildparser()
+    parser = buildparser()
     params = parser.parse_args()
 
     # Process in a temporary folder that will be automatically removed after exit
     with tempfile_tempdir() as tmpdirname:
 
-        # Prepare arguments for the tool
-        args = [
-            '-rules_type', params.rules_type,
-            '-output', tmpdirname,
-            '-diameters', params.diameters
-            ]
-
         # Run the tool
-        result = tool_entrypoint(args)
+        result = RetroRules(params.rules_type, tmpdirname, params.diameters)
 
+        # Copy results to the place expected by Galaxy 
         shutil_copy(result, params.output)
 
 
