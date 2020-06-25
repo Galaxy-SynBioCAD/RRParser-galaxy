a93d18a57ec4c322d31d9e54c2760fe3f7f73701
joan <joan.herisson@univ-evry.fr>
2020-06-25 18:39:11 +0200

    tar

--- run.py
+++ run.py
@@ -20,14 +20,9 @@ if __name__ == "__main__":
     # Process in a temporary folder that will be automatically removed after exit
     with tempfile_tempdir() as tmpdirname:
 
+        if params.output_format=='tar': params.output_format+='.gz'
         # Run the tool
         result_file = rules(params.rule_type, tmpdirname, params.diameters, params.output_format)
 
         # Copy results to the place expected by Galaxy
         shutil_copy(result_file, params.output_folder)
-
-
-        # # Format ouput data as expected by Galaxy
-        # with tarfile_open(params.output, mode='w:gz') as tf:
-        #     for name in os_listdir(tmpdirname):
-        #         tf.add(tmpdirname+"/"+name, arcname=name)
