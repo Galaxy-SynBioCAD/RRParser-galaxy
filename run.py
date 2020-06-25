2605ff3350a2ce61065277f012fb9ec6001b39e6
joan <joan.herisson@univ-evry.fr>
2020-06-25 19:22:01 +0200

    tar issues

--- run.py
+++ run.py
@@ -15,12 +15,15 @@ if __name__ == "__main__":
 
     # Parse arguments with the tool parser
     parser = build_parser()
+    parser.add_argument('--output_format_galaxy',
+                            type=str,
+                            choices=['csv', 'tar'])
     params = parser.parse_args()
 
     # Process in a temporary folder that will be automatically removed after exit
     with tempfile_tempdir() as tmpdirname:
 
-        if params.output_format=='tar': params.output_format += '.gz'
+        if params.output_format_galaxy=='tar': params.output_format = 'tar.gz'
         # Run the tool
         result_file = rules(params.rule_type, tmpdirname, params.diameters, params.output_format)
 
